edc1aaf7c03d4b02d915399c7dfe89d1a7d1b5f1
nikosgrigo <iis21084@uom.edu.gr>
2023-12-08 16:21:59 +0200

    Update some functions

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
 from flask import Flask,request,Response
 import csv,json,datetime
+import pandas as pd
 
 app = Flask(__name__)
 
-def read_file(path,mode ='r'):
-    with open(path, mode = mode)as file:
+def read_file(path):
+    with open(path, mode = 'r')as file:
         data = csv.DictReader(file)
         # for lines in data:
         #     print(lines)
         return list(data)
     
-def write_file(path,row,mode = 'a'):
-   with open(path, mode, newline='') as f:
+
+def write_file(path,row):
+   with open(path, mode = 'a', newline='') as f:
       writer = csv.writer(f, delimiter=',')
       writer.writerow(row)
 
-all_books_info = read_file('./data/Books.csv','r')
-all_rating_info = read_file('./data/Ratings.csv','r')
+
+all_books_info = read_file('./data/Books.csv')
+all_rating_info = read_file('./data/Ratings.csv')
+all_rented_books = read_file('./data/RentedBooks.csv')
+
 # print(all_rating_info)
 
 def get_book_from_list_by_identifier(identifier:str,list,value):
    result = []
+
+   if identifier == 'ISBN':
+      for book in list:
+         if book.get(identifier) == value:
+            result.append(book)
+            break
+
    for book in list:
          if book.get(identifier) == value:
             result.append(book)
-            if identifier == 'ISBN':
-               break
    return result
 
 
-def calculate_rental_fee(days:str):
-   days = int(days)
+def days_between(d1, d2):
+   dt0 = pd.to_datetime(d1, format = '%Y-%m-%d')
+   dt1 = pd.to_datetime(d2, format = '%Y-%m-%d')
+   return (dt1 - dt0).days
+
+
+def calculate_rental_fee(start,end):
+
+   days = days_between(start,end)
+   print(days)
+   
    if days <= 3:
       return days*1
    else:
@@ -38,10 +57,11 @@ def calculate_rental_fee(days:str):
 
 
 def send_response(response_data,status:str,status_code:int):
-   if status == 'success' and type(response_data) == list:
+   if status == 'success' and not type(response_data) == str:
       return Response(json.dumps({"data":response_data,"status":status,"status code":status_code}), content_type='application/json', status=status_code)
    else:
       return Response(json.dumps({"message":response_data,"status":status,"status code":status_code}), content_type='application/json', status=status_code)
+
 
 #-------------BUG--------------
 def get_all_books_from_list():
@@ -65,55 +85,6 @@ def get_all_books_from_list():
       # print(all_books)
       return all_books
 
-# Retrieve a list of all available books
-@app.route('/books',methods = ['GET'])
-def get_all_books():
-   if request.method == 'GET':
-      data = get_all_books_from_list()
-      return send_response(data,"success",200)
-
-# Retrieve a list of available books based on a specified category.
-@app.route('/books/author/<author>',methods = ['GET'])
-def get_all_books_by_author(author):
-   if request.method == 'GET':
-      books = get_all_books_from_list()
-      data = get_book_from_list_by_identifier('Book-Author',books,author)
-      if data:
-         return send_response(data,"success",200)
-      return send_response("Books not found","error",404)
-               
-   
-# Retrieve detailed information about a specific book.
-@app.route('/book/<id>',methods = ['GET'])
-def get_all_books_by_id(id):
-   if request.method == 'GET':
-         books = get_all_books_from_list()
-         data = get_book_from_list_by_identifier('ISBN',books,id)
-         if data:
-            return send_response(data,"success",200)         
-         return send_response("Books not found","error",404)
-               
-# Retrieve a list of available books that was released by a specific publisher
-@app.route('/books/publisher/<publisher>',methods = ['GET'])
-def get_all_books_by_publisher(publisher):
-   if request.method == 'GET':
-         books = get_all_books_from_list()
-         data = get_book_from_list_by_identifier('Publisher',books,publisher)
-         if data:
-            return send_response(data,"success",200)
-         return send_response("Books not found","error",404)
-
-#Retrieve a list of available books that was released on a specific year or between certain dates (e.g., from 2000 to 2005)
-@app.route('/books/date/<date>',methods = ['GET'])
-def get_all_books_by_date(date):
-    if request.method == 'GET':
-         date = str(date)
-         books = get_all_books_from_list()
-         data = get_book_from_list_by_identifier('Year-Of-Publication',books,date)
-         if data:
-            return send_response(data,"success",200)
-         return send_response("Books not found","error",404)
-
 def is_book_rented(list):
    filename = './data/RentedBooks.csv'
    flag = False
@@ -122,19 +93,20 @@ def is_book_rented(list):
       datareader = csv.reader(csvfile)
       next(datareader, None)     #skip first line - columns
       for row in datareader:
-         if list[0] == row[0]:      #check if book is already inside the rented Books
+         if list[0] == row[0]:      #check if book is already inside the rented Books and flag column is True
             flag = True
             break
    return flag
 
-def rent_book(data,days):
-   #Add new column-field for days
-   data.append(days)
+def rent_book(data):
 
    #Create new timestamp
    date = datetime.datetime.now()
    # Format as "YYYY-MM-DD"
    formatted_date = date.strftime('%Y-%m-%d')
+
+   # flag = True
+   # data.append(flag)
 
    #Add new column-field for date
    data.append(formatted_date)
@@ -142,13 +114,68 @@ def rent_book(data,days):
    #Add new row to file or DB
    write_file("./data/RentedBooks.csv",data)
 
+def calculate_total_rental_fee(rented_book_list):
+   return sum(calculate_rental_fee(int(rental['Days'])) for rental in rented_book_list)
+
+def get_book_list_by_range(data,start,end):
+   list = []
+   for book in data:
+      if book.get('Date') >=start and book.get('Date') <=end:
+         list.append(book)
+   return list
+
+
+books = get_all_books_from_list()
+
+# Retrieve a list of all available books
+@app.route('/books',methods = ['GET'])
+def get_all_books():
+   return send_response(books,"success",200)
+
+
+# Retrieve a list of available books based on a specified category.
+@app.route('/books/author/<author>',methods = ['GET'])
+def get_all_books_by_author(author):
+   data = get_book_from_list_by_identifier('Book-Author',books,author)
+   if data:
+      return send_response(data,"success",200)
+   return send_response("Books not found","error",404)
+   
+               
+# Retrieve detailed information about a specific book.
+@app.route('/book/<id>',methods = ['GET'])
+def get_all_books_by_id(id):
+   data = get_book_from_list_by_identifier('ISBN',books,id)
+   if data:
+      return send_response(data,"success",200)         
+   return send_response("Books not found","error",404)
+
+
+# Retrieve a list of available books that was released by a specific publisher
+@app.route('/books/publisher/<publisher>',methods = ['GET'])
+def get_all_books_by_publisher(publisher):
+   data = get_book_from_list_by_identifier('Publisher',books,publisher)
+   if data:
+      return send_response(data,"success",200)
+   return send_response("Books not found","error",404)
+
+
+#Retrieve a list of available books that was released on a specific year or between certain dates (e.g., from 2000 to 2005)
+@app.route('/books/date/<date>',methods = ['GET'])
+def get_all_books_by_date(date):
+   date = str(date)
+   data = get_book_from_list_by_identifier('Year-Of-Publication',books,date)
+   if data:
+      return send_response(data,"success",200)
+   return send_response("Books not found","error",404)
+
+
+
 #Rent a book, making it unavailable for others to rent.
-@app.route('/rent/<id>/<days>',methods = ['POST'])
-def set_book_as_rented(id,days):
+@app.route('/rent/<id>',methods = ['POST'])
+def set_book_as_rented(id):
    #1.find book
-   if request.method == 'POST':
          result = {}
-         books = get_all_books_from_list()
          for book in books:
             if book.get("ISBN") == id:
                #2.retrieve book from the general book list
@@ -158,17 +185,17 @@ def set_book_as_rented(id,days):
          data = []
          for value in result.values():
             data.append(value)
-         if data:    # check if data != None
-            # print(data)
+         if data:   
             #check if book is already rented
             if is_book_rented(data):
                return send_response("Book not available for rent","error",400)
             else:
                #3.add book on another file or table
-               # days = 3
-               rent_book(data,days)
+
+               rent_book(data)
                return send_response("Book rented successfully","success",200)
    
+
 # Return a rented book and calculate the rental fee based on the number of days rented.
 @app.route('/return/<id>',methods = ['PUT'])
 def get_rented_book(id):
@@ -186,34 +213,50 @@ def get_rented_book(id):
                data = row
                break
       if(data):
-      #2.Calculate rental fee based on rented days
-         rented_days = data[-2]
-         rental_fee = calculate_rental_fee(rented_days)
+         rented_date = data[-1]              #get rented date 
+         date = datetime.datetime.now()      #return book now so create a new date
+         end_date = date.strftime('%Y-%m-%d')
+
+        #2.Calculate rental fee based on rented days
+         rental_fee = calculate_rental_fee(rented_date,end_date)
+
+         #Change flag=False to make book avaliable for others
+
          #3.Return response with the rental fee
          return Response(json.dumps({"status":"success","status code":200,"message": "Book returned successfully", "rental_fee": rental_fee}), content_type='application/json', status=200)
       return Response(json.dumps({"status":"error","status code":400,"message": "Book not found or not currently rented"}), content_type='application/json', status=400)
 
-def get_book_list_by_range(data,start,end):
-   list = []
-   for book in data:
-      if book.get('Date') >=start and book.get('Date') <=end:
-         list.append(book)
-   return list
 
 # Retrieve a list of books that were rented within a specified date range.
 @app.route('/rentals',methods = ['GET'])
 def get_all_rented_books_for_period():
-
-   if request.method == 'GET':
       start_date = request.args.get('start') #(i.e. ?start=YYYY-MM-DD)
       end_date = request.args.get('end')     #(i.e. &end=YYYY-MM-DD)
       if start_date is None and end_date is None:     #(+check if start date is smaller than end date)
          print("Argument not provided")
          return send_response("Arguments not found","error",404)
       else:
-         all_rented_books = read_file('./data/RentedBooks.csv','r')
          data = get_book_list_by_range(all_rented_books,start_date,end_date)
          return send_response(data,"success",200) 
+
+
+#Calculate and retrieve the total revenue generated by book rentals within a specified date range.
+@app.route('/revenue',methods = ['GET'])
+def get_total_revenue():
+      start_date = request.args.get('start') #(i.e. ?start=YYYY-MM-DD)
+      end_date = request.args.get('end')     #(i.e. &end=YYYY-MM-DD)
+      if start_date is None and end_date is None:     #(+check if start date is smaller than end date)
+         print("Argument not provided")
+         return send_response("Arguments not found","error",404)
+      else:
+         books_rented_within_range = get_book_list_by_range(all_rented_books,start_date,end_date)
+         # print(books_rented_within_range)
+         data = calculate_total_rental_fee(books_rented_within_range)
+         return send_response(data,"success",200) 
       
+
 if __name__ == '__main__':
+   #Read all nessecary files and keep into a dictionary (better than list)
+
    app.run(debug=True)
+
